2195fd3e2b20245aa1d08979a1b607d03b9df418
flerka <marya.kozhemyakina@gmail.com>
2020-02-10 14:29:13 +0200

    update worktree commands

--- git/git-worktree.cmd
+++ git/git-worktree.cmd
@@ -1,1 +1,8 @@
-git worktree add 
+Add worktree
+git worktree add ../<folder-name-where-worktree-created> <branch-name-you-want-to-work>
+
+Remove worktree
+git worktree remove ../<worktree-folder-name>
+
+Worktree list
+git worktree all
