799833009abf0f016b1f077aec7700d3297a1c74
flerka <6013716+flerka@users.noreply.github.com>
2020-02-13 19:21:55 +0200

    update worktree formatting

--- git/git-worktree.cmd
+++ git/git-worktree.cmd
@@ -1,14 +1,17 @@
 # Add worktree
+
 ```
 git worktree add ../<folder-name-where-worktree-created> <branch-name-you-want-to-work>
 ```
 
 # Remove worktree
+
 ```
 git worktree remove ../<worktree-folder-name>
 ```
 
 # Worktree list
+
 ```
 git worktree all
 ```
